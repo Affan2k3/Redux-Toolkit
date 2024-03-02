e39e2ee170b2e0a49ff1392d1942e639cc9b42e7
Affan Ahmed Khan <affan2k3@gmail.com>
2024-03-02 21:24:15 +0500

    step 9

--- src/app/components/DisplayUsers.tsx
+++ src/app/components/DisplayUsers.tsx
@@ -1,9 +1,18 @@
+"use client";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function DisplayUsers() {
+  const userData = useSelector((data: any) => data.users);
+  console.log(userData);
   return (
-    <div>
+    <div className="w-[30%]">
       <h2 className="font-semibold text-[20px] mt-10">User List</h2>
+      {userData.map((i: any) => (
+        <div key={i.id} className="bg-blue-200 w-full py-2 px-2 mt-2">
+          {i.name}
+        </div>
+      ))}
     </div>
   );
 }
