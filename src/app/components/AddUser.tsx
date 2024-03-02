e77408c42099121d59d127bb31b8c1bd93545fdd
Affan Ahmed Khan <affan2k3@gmail.com>
2024-03-02 21:03:25 +0500

    step 7th

--- src/app/components/AddUser.tsx
+++ src/app/components/AddUser.tsx
@@ -1,6 +1,13 @@
-import React from "react";
-
-export default function AddUser() {
+"use client";
+import React, { useState } from "react";
+import { addUser } from "../redux/slice";
+import { useDispatch } from "react-redux";
+export default function AddUsers() {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+  const userDispatch = () => {
+    dispatch(addUser(name));
+  };
   return (
     <div>
       <h3 className="text-[20px] font-semibold">Add User</h3>
@@ -8,8 +15,16 @@ export default function AddUser() {
         type="text"
         placeholder="Add a User"
         className="border border-black py-1 px-2"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
       />
-      <button className="border border-green-800 py-1 ml-2 px-1 my-2">
+      <button
+        className=" border border-green-800 py-1 ml-2 px-1 my-2"
+        onClick={() => {
+          userDispatch();
+        }}
+      >
         Add User
       </button>
     </div>
